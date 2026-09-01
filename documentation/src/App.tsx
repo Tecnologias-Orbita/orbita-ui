d6bd81cc91c066f031654fe38f22b629e2c26e76
Lazaro <lachy200408@gmail.com>
2026-09-01 15:28:27 -0500

    add(doc): Testing the library

--- documentation/src/App.tsx
+++ documentation/src/App.tsx
@@ -1,4 +1,9 @@
-import { Btn, Navbar } from "@orbita-ui/react";
+import {
+  Btn,
+  Footer,
+  Navbar,
+  Sidebar,
+} from "@tecnologias-orbita/orbita-ui-react";
 import { useState } from "react";
 
 export default function App() {
@@ -23,12 +28,64 @@ export default function App() {
           </Navbar.Item>
         </Navbar.List>
       </Navbar.Container>
-      <div className="grid h-screen place-items-center bg-gray-100">
-        <h1 className="text-7xl">Hola</h1>
-        <Btn onClick={() => setState((i) => i + 1)}>
-          Presiona para decir hola {state} {state > 1 ? "veces" : "vez"}
-        </Btn>
-      </div>
+      <Sidebar.Wrapper>
+        <Sidebar.Sidebar
+          collapseMode="full"
+          animated
+          collapsedSwitchButton={(onCollapse: () => void) => (
+            <Btn
+              onClick={onCollapse}
+              className="bg-red-50 text-red-800 border-red-800 hover:bg-red-100"
+            >
+              Cerrar
+            </Btn>
+          )}
+        >
+          Hola
+        </Sidebar.Sidebar>
+        <Sidebar.Main>
+          <div className="grid h-screen place-items-center bg-gray-100">
+            <h1 className="text-7xl">Hola</h1>
+            <Btn onClick={() => setState((i) => i + 1)}>
+              Presiona para decir hola {state} {state > 1 ? "veces" : "vez"}
+            </Btn>
+          </div>
+        </Sidebar.Main>
+        <Sidebar.Sidebar side="right" collapseMode="reduced" animated>
+          {(isCollapsed: boolean) => (
+            <>Hola {isCollapsed ? "colapsado" : "abierto"}</>
+          )}
+        </Sidebar.Sidebar>
+      </Sidebar.Wrapper>
+      <Footer.Container centered>
+        <Footer.Grid className="w-full max-w-5xl text-sm">
+          <Footer.Column width="50%">
+            <Footer.Item href="/">Inicio</Footer.Item>
+            <Footer.Item href="/components">Componentes</Footer.Item>
+            <Footer.Item
+              href="https://github.com/Tecnologias-Orbita/orbita-ui"
+              target="_blank"
+            >
+              GitHub
+            </Footer.Item>
+          </Footer.Column>
+          <Footer.Column width="50%">
+            <Footer.Item href="/utilities">Utilidades</Footer.Item>
+            <Footer.Item href="/utilities">Utilidades</Footer.Item>
+          </Footer.Column>
+        </Footer.Grid>
+        <Footer.Bottom
+          centered
+          copyright={{
+            name: "Tecnologías Órbita",
+            year: "2026",
+          }}
+        >
+          <p className="text-sm opacity-70">
+            Hecho con cariño por Tecnologías Órbita
+          </p>
+        </Footer.Bottom>
+      </Footer.Container>
     </>
   );
 }
